16460d49af20ff65eb848660f30e25f77b4b85bc
rayray1010 <da866810@gmail.com>
2021-09-09 21:27:23 +0800

    Create show pages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
-
+// activate handlebars
 app.engine(
   'handlebars',
   exphbs({
@@ -14,10 +14,21 @@ app.engine(
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
+// render index page
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+//show each page detail
+app.get('/restaurants/:restaurant', (req, res) => {
+  const restaurantId = req.params.restaurant
+  const restaurant = restaurantList.results.find((restaurant) => {
+    return restaurant.id.toString() === restaurantId
+  })
+  res.render('show', { restaurant })
+})
+
+// listen sever
 app.listen(port, () => {
   console.log('本機伺服器啟動，路由為：http://localhost:3000')
 })
