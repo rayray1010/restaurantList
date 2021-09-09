623194d006b1ade2e7a6a005a66a697978c043ef
rayray1010 <da866810@gmail.com>
2021-09-09 17:00:39 +0800

    New views

---
+++ app.js
@@ -0,0 +1,21 @@
+//creat variables
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+  })
+)
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('本機伺服器啟動，路由為：http://localhost:3000')
+})
